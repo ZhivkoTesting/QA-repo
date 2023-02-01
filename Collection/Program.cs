05014e345bbd54f95efab115faa1dd8f4f60cd87
Zhivko Ivanov <zhivkozz85@gmail.com>
2023-02-01 21:51:41 +0200

    Edit

--- Collection/Program.cs
+++ Collection/Program.cs
@@ -1,22 +1,68 @@
-﻿using Collections;
+﻿using System;
 
-namespace Collection
+namespace Collections
 {
-    internal class Program
+    class CollectionsExamples
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            var collection = new Collection<int>();
-            Console.WriteLine(collection.Count);
-            Console.WriteLine(collection.Capacity);
-            collection.Add(5);
-            Console.WriteLine(collection);
-            Console.WriteLine(collection.Count);
-            Console.WriteLine(collection.Capacity);
-            collection.Add(10);
-            Console.WriteLine(collection);
-            Console.WriteLine(collection.Count);
-            Console.WriteLine(collection.Capacity);
+            // Console.WriteLine();
+            // Console.WriteLine("Collection<T> Examples");
+            // Console.WriteLine("======================");
+            // Console.WriteLine();
+
+            Collection<int> nums = new Collection<int>();
+            Console.WriteLine($"Empty collection: {nums}");
+            Console.WriteLine($"Count: {nums.Count}. Capacity: {nums.Capacity}");
+            Console.WriteLine();
+
+             nums = new Collection<int>(new int[] { 10, 20, 30, 40, 50 });
+             Console.WriteLine(nums);
+             Console.WriteLine($"Count: {nums.Count}. Capacity: {nums.Capacity}");
+             Console.WriteLine();
+
+            // nums.Add(60);
+            // Console.WriteLine("Added: 60");
+            // Console.WriteLine(nums);
+            // Console.WriteLine();
+
+            // nums[1] = 2000;
+            // nums[5] = 6000;
+            // Console.WriteLine("Changed: nums[1] and nums[5]");
+            // Console.WriteLine(nums);
+            // Console.WriteLine();
+
+            // Console.WriteLine($"num[0] = {nums[0]}");
+            // Console.WriteLine($"num[3] = {nums[3]}");
+            // Console.WriteLine();
+
+            // var removedItem = nums.RemoveAt(0);
+            // Console.WriteLine($"Removed item from position #0. Value = {removedItem}");
+            // Console.WriteLine(nums);
+            // Console.WriteLine();
+
+            // removedItem = nums.RemoveAt(4);
+            // Console.WriteLine($"Removed item from position #4. Value = {removedItem}");
+            // Console.WriteLine(nums);
+            // Console.WriteLine();
+
+            // nums.Exchange(0, 1);
+            // Console.WriteLine("Exchanged positions #0 and #1");
+            // Console.WriteLine(nums);
+            // Console.WriteLine();
+
+            // for (int i = 1; i <= 20; i++)
+            //     nums.Add(i);
+            // Console.WriteLine("Added numbers [1...20]");
+            // Console.WriteLine(nums);
+            // Console.WriteLine();
+
+            // nums.InsertAt(0, 10);
+            // nums.InsertAt(1, 15);
+            // Console.WriteLine("Inserted 10 at the start and 15 after it");
+            // Console.WriteLine(nums);
+            // Console.WriteLine($"Count: {nums.Count}. Capacity: {nums.Capacity}");
+            // Console.WriteLine();
         }
     }
 }
--- CollectionTests/UnitTest1.cs
+++ CollectionTests/UnitTest1.cs
@@ -10,10 +10,49 @@ namespace CollectionTests
         }
 
         [Test]
-        public void Test1()
+        public void showEmptyCollection()
         {
             var collection = new Collection<int>();
             Assert.AreEqual(collection.ToString(), "[]");
         }
+
+        [Test]
+        public void initialCount()
+        {
+            var collection = new Collection<int>();
+            Assert.AreEqual(collection.Count, 0);
+        }
+        [Test]
+        public void initialCapacity()
+        {
+            var collection = new Collection<int>();
+            Assert.That(collection.Capacity, Is.EqualTo(16));
+        }
+        [Test]
+        public void addFirstItem()
+        {
+            var collection = new Collection<int>();
+            collection.Add(5);
+            Assert.That(collection.Capacity, Is.EqualTo(16));
+            Assert.That("[5]", Is.EqualTo(collection.ToString()));
+        }
+
+        //  
+
+        [Test]
+        public void addMultyItems()
+        {
+            var collection = new Collection<string>("Cat", "Dog");
+            collection.Add("Rabit");
+            Assert.That("[Cat, Dog, Rabit]", Is.EqualTo(collection.ToString()));
+        }
+        [Test]
+        public void removeAtPosition()
+        {
+            var collection = new Collection<string>("Rabbit", "Snake", "Frog");
+            collection.RemoveAt(1);
+            Assert.That("[Rabbit, Frog]", Is.EqualTo(collection.ToString()));
+        }
+
     }
 }
