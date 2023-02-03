87f7d65f704fee08bbefb686be2d5803f411c7b4
Melinda <melaniyosifova@gmail.com>
2023-02-03 18:15:06 +0200

    Corection

---
+++ MultiplierTests/TestsBase.cs
@@ -0,0 +1,13 @@
+﻿namespace MultiplierTests
+{
+    public class TestsBase
+    {
+
+        [Test]
+        public void MultiplTreeNumbers()
+        {
+            var actual = Sumator.Multiplier.multi(new int[] { 1, 2, 3, 3 });
+            Assert.That(actual, Is.EqualTo(18));
+        }
+    }
+}
